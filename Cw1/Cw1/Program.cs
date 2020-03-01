b6a5fcbbd53f38d85e0b287f4de98e684112e78d
Mateusz Stasiak <s17518@pjwstk.edu.pl>
2020-03-01 19:07:18 +0100

    first commit

---
+++ Cw1/Cw1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Uri siteUri = new Uri(args[0]);
+            var httpClient = new HttpClient();
+            var respone = await httpClient.GetAsync(siteUri);
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(respone.Headers);
+        }
+    }
+}
