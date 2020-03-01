432a757560709377ec9bd0ac5d5b7edae2062d59
s17518 <s17518@pjwstk.edu.pl>
2020-03-01 19:17:16 +0100

    working

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Cw1
@@ -12,7 +13,14 @@ namespace Cw1
             var httpClient = new HttpClient();
             var respone = await httpClient.GetAsync(siteUri);
             Console.WriteLine("Hello World!");
-            Console.WriteLine(respone.Headers);
+            //Console.WriteLine(respone.Content.ReadAsStringAsync());
+            var content = await respone.Content.ReadAsStringAsync();
+            Regex regex = new Regex(@"[a-zA-z0-9.]+\@[a-zA-Z0-9.]+\.[a-zA-Z0-9.]+");
+            MatchCollection matcher = regex.Matches(content);
+            foreach (Match match in matcher)
+            {
+                Console.WriteLine(match);
+            }
         }
     }
 }
